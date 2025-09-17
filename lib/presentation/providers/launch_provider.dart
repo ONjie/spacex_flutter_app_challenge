19345221622450a8119ade049a3b7558eb11e0cc
Muhammed O Njie <muhammedonjie16@gmail.com>
2025-09-17 19:02:06 +0000

    Implement LaunchProvider with state management and data fetching methods

--- lib/presentation/providers/launch_provider.dart
+++ lib/presentation/providers/launch_provider.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:spacex_flutter_app/domain/entities/launch_entity.dart';
+import 'package:spacex_flutter_app/domain/use_cases/fetch_launch_by_id_use_case.dart';
+import 'package:spacex_flutter_app/domain/use_cases/fetch_launches_use_case.dart';
 
 // TODO: Implement LaunchProvider
 // This is a placeholder for the launch state management
@@ -9,27 +12,87 @@ import 'package:flutter/material.dart';
 // 4. State management for launches list, loading, error states
 
 class LaunchProvider extends ChangeNotifier {
+  final FetchLaunchesUseCase fetchLaunchesUseCase;
+  final FetchLaunchByIdUseCase fetchLaunchByIdUseCase;
+
+  LaunchProvider({
+    required this.fetchLaunchesUseCase,
+    required this.fetchLaunchByIdUseCase,
+  });
   // TODO: Add launch-related state variables
-  // List<Launch> _launches = [];
-  // bool _isLoading = false;
-  // String? _error;
+  List<LaunchEntity> _launches = [];
+  LaunchEntity? _launch;
+  bool _isLoading = false;
+  String? _error;
+
+  //TODO: Add setters
+  void _setLoading({required bool loading}) {
+    _isLoading = loading;
+    notifyListeners();
+  }
+
+  void _setError({required String? error}) {
+    _error = error;
+    notifyListeners();
+  }
 
   // TODO: Add getters
-  // List<Launch> get launches => _launches;
-  // bool get isLoading => _isLoading;
-  // String? get error => _error;
+  List<LaunchEntity> get launches => _launches;
+  LaunchEntity? get launch => _launch;
+  bool get isLoading => _isLoading;
+  String? get error => _error;
 
   // TODO: Add methods
-  // Future<void> fetchLaunches()
-  // Future<void> fetchUpcomingLaunches()
-  // Future<void> fetchPastLaunches()
-  // Future<void> fetchLaunchById(String id)
-  // void clearError()
-  // void refreshLaunches()
+  Future<void> fetchLaunches() async {
+    _setLoading(loading: true);
+    _setError(error: null);
+
+    final launchesOrFailure = await fetchLaunchesUseCase.call();
+
+    launchesOrFailure.fold(
+      (failure) {
+        _setError(error: failure.message);
+      },
+      (launches) {
+        _launches = launches;
+        notifyListeners();
+      },
+    );
+    _setLoading(loading: false);
+  }
+
+  Future<void> fetchLaunchById({required String id}) async {
+    _setLoading(loading: true);
+    _setError(error: null);
+
+    final launchOrFailure = await fetchLaunchByIdUseCase.call(id: id);
+
+    launchOrFailure.fold(
+      (failure) {
+        _setError(error: failure.message);
+      },
+      (launch) {
+        _launch = launch;
+        notifyListeners();
+      },
+    );
+    _setLoading(loading: false);
+  }
+
+  void clearError() {
+    _setError(error: null);
+  }
+
+  void refreshLaunches() async {
+    await fetchLaunches();
+  }
 
   @override
   void dispose() {
-    // TODO: Clean up resources
+    _launches = [];
+    _launch = null;
+    _isLoading = false;
+    _error = null;
     super.dispose();
   }
 }
