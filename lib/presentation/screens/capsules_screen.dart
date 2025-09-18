eb265c6e12509ebab5032b7a166818137813d234
Muhammed O Njie <muhammedonjie16@gmail.com>
2025-09-18 22:02:44 +0000

    Added CapsulesScreen widget with a placeholder

---
+++ lib/presentation/screens/capsules_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CapsulesScreen extends StatelessWidget {
+  const CapsulesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
