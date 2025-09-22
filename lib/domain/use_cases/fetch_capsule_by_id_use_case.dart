5bd60b20399fa7342e363ec362be0e6bb4726134
Muhammed O Njie <muhammedonjie16@gmail.com>
2025-09-22 20:53:56 +0000

    added comments

--- lib/domain/use_cases/fetch_capsule_by_id_use_case.dart
+++ lib/domain/use_cases/fetch_capsule_by_id_use_case.dart
@@ -4,6 +4,9 @@ import 'package:spacex_flutter_app/domain/entities/capsule_entity.dart';
 import '../../core/failures/failures.dart';
 import '../repositories/space_x_repository.dart';
 
+// Use case for fetching a single capsule by its id.
+// This class abstracts the business logic from the repository layer.
+
 class FetchCapsuleByIdUseCase {
   final SpaceXRepository spaceXRepository;
 
