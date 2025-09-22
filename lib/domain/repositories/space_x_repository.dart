2df190bd2bed4fa67b2ce5899fa6ad3c6bf18ddd
Muhammed O Njie <muhammedonjie16@gmail.com>
2025-09-22 20:53:36 +0000

    added pagination methods for capsules and launches and added comments

--- lib/domain/repositories/space_x_repository.dart
+++ lib/domain/repositories/space_x_repository.dart
@@ -5,6 +5,9 @@ import 'package:spacex_flutter_app/domain/entities/rocket_entity.dart';
 
 import '../../core/failures/failures.dart';
 
+// Abstract repository defining all SpaceX-related data operations.
+// This allows decoupling the data layer from the domain and presentation layers.
+
 abstract class SpaceXRepository {
   Future<Either<Failure, List<RocketEntity>>> fetchRockets();
   Future<Either<Failure, RocketEntity>> fetchRocketById({required String id});
@@ -12,4 +15,12 @@ abstract class SpaceXRepository {
   Future<Either<Failure, CapsuleEntity>> fetchCapsuleById({required String id});
   Future<Either<Failure, List<LaunchEntity>>> fetchLaunches();
   Future<Either<Failure, LaunchEntity>> fetchLaunchById({required String id});
+  Future<Either<Failure, List<LaunchEntity>>> fetchLaunchesByPagination({
+    required int offset,
+    required int limit,
+  });
+  Future<Either<Failure, List<CapsuleEntity>>> fetchCapsulesByPagination({
+    required int offset,
+    required int limit,
+  });
 }
