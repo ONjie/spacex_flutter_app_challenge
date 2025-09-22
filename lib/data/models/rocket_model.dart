27705f26462581f7dedb4318ab6379f11d381d16
Muhammed O Njie <muhammedonjie16@gmail.com>
2025-09-22 20:20:07 +0000

    Refactored RocketModel by removing company and country properties, and added numberOfEngines for improved data structure

--- lib/data/models/rocket_model.dart
+++ lib/data/models/rocket_model.dart
@@ -7,9 +7,7 @@ class RocketModel extends Equatable {
     required this.id,
     required this.active,
     required this.boosters,
-    required this.company,
     required this.costPerLaunch,
-    required this.country,
     required this.description,
     required this.diameterInFeet,
     required this.diameterInMeters,
@@ -22,6 +20,7 @@ class RocketModel extends Equatable {
     required this.stages,
     required this.successRate,
     required this.type,
+    required this.numberOfEngines,
   });
 
   final String id;
@@ -40,34 +39,31 @@ class RocketModel extends Equatable {
   final int massInLb;
   final DateTime firstFlight;
   final String description;
-  final String company;
-  final String country;
+  final int numberOfEngines;
 
   /// Create a RocketModel from JSON Map
   factory RocketModel.fromJson(Map<String, dynamic> json) {
     return RocketModel(
-      id: json['id'] as String,
-      name: json['name'] as String,
-      type: json['type'] as String,
-      active: json['active'] as bool,
-      stages: (json['stages'] as num).toInt(),
-      boosters: (json['boosters'] as num).toInt(),
-      costPerLaunch: (json['cost_per_launch'] as num).toInt(),
-      successRate: (json['success_rate_pct'] as num).toInt(),
-      country: json['country'] as String,
-      company: json['company'] as String,
-      description: json['description'] as String,
-      heightInFeet: (json['height']['feet'] as num).toDouble(),
-      heightInMeters: (json['height']['meters'] as num).toDouble(),
-      diameterInFeet: (json['diameter']['feet'] as num).toDouble(),
-      diameterInMeters: (json['diameter']['meters'] as num).toDouble(),
-      massInKg: (json['mass']['kg'] as num).toInt(),
-      massInLb: (json['mass']['lb'] as num).toInt(),
-      firstFlight: DateTime.parse(json['first_flight']),
-    );
+        id: json['id'] as String,
+        name: json['name'] as String,
+        type: json['type'] as String,
+        active: json['active'] as bool,
+        stages: (json['stages'] as num).toInt(),
+        boosters: (json['boosters'] as num).toInt(),
+        costPerLaunch: (json['cost_per_launch'] as num).toInt(),
+        successRate: (json['success_rate_pct'] as num).toInt(),
+        description: json['description'] as String,
+        heightInFeet: (json['height']['feet'] as num).toDouble(),
+        heightInMeters: (json['height']['meters'] as num).toDouble(),
+        diameterInFeet: (json['diameter']['feet'] as num).toDouble(),
+        diameterInMeters: (json['diameter']['meters'] as num).toDouble(),
+        massInKg: (json['mass']['kg'] as num).toInt(),
+        massInLb: (json['mass']['lb'] as num).toInt(),
+        firstFlight: DateTime.parse(json['first_flight']),
+        numberOfEngines: (json['engines']['number'] as num).toInt());
   }
 
- /// Converts RocketModel to JSON Map
+  /// Converts RocketModel to JSON Map
   Map<String, dynamic> toJson() => {
         'id': id,
         'name': name,
@@ -77,8 +73,6 @@ class RocketModel extends Equatable {
         'boosters': boosters,
         'cost_per_launch': costPerLaunch,
         'success_rate_pct': successRate,
-        'country': country,
-        'company': company,
         'description': description,
         'height': {
           'feet': heightInFeet,
@@ -93,8 +87,8 @@ class RocketModel extends Equatable {
           'lb': massInLb,
         },
         'first_flight': firstFlight.toIso8601String(),
+        'engines': {'number': numberOfEngines}
       };
-
 
   /// Converts RocketModel to RocketEntity
   /// This is used to pass data from the data layer to the domain layer
@@ -102,9 +96,7 @@ class RocketModel extends Equatable {
         id: id,
         active: active,
         boosters: boosters,
-        company: company,
         costPerLaunch: costPerLaunch,
-        country: country,
         description: description,
         diameterInFeet: diameterInFeet,
         diameterInMeters: diameterInMeters,
@@ -117,27 +109,27 @@ class RocketModel extends Equatable {
         stages: stages,
         successRate: successRate,
         type: type,
+        numberOfEngines: numberOfEngines,
       );
 
   @override
   List<Object?> get props => [
-     id,
-    name,
-    type,
-    active,
-    stages,
-    boosters,
-    costPerLaunch,
-    successRate,
-    heightInFeet,
-    heightInMeters,
-    diameterInFeet,
-    diameterInMeters,
-    massInKg,
-    massInLb,
-    firstFlight,
-    description,
-    company,
-    country
-  ];
+        id,
+        name,
+        type,
+        active,
+        stages,
+        boosters,
+        costPerLaunch,
+        successRate,
+        heightInFeet,
+        heightInMeters,
+        diameterInFeet,
+        diameterInMeters,
+        massInKg,
+        massInLb,
+        firstFlight,
+        description,
+        numberOfEngines
+      ];
 }
