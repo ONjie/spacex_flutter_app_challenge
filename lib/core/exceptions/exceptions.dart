fa888d3e0223676ba1e04b0ec09a1cf8eacbafbf
Muhammed O Njie <muhammedonjie16@gmail.com>
2025-09-22 20:19:18 +0000

    Added documentation comment for GraphQLException class

--- lib/core/exceptions/exceptions.dart
+++ lib/core/exceptions/exceptions.dart
@@ -1,3 +1,4 @@
+// A custom exception class to represent GraphQL-related errors.
 class GraphQLException implements Exception {
   final String message;
   GraphQLException({required this.message});
