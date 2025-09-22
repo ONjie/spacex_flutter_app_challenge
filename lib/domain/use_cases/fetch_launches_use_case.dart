6596a3d3da95f61c7032a58774c54f2330c7a9ed
Muhammed O Njie <muhammedonjie16@gmail.com>
2025-09-22 20:55:58 +0000

    added comments

--- lib/domain/use_cases/fetch_launches_use_case.dart
+++ lib/domain/use_cases/fetch_launches_use_case.dart
@@ -4,6 +4,9 @@ import 'package:spacex_flutter_app/domain/entities/launch_entity.dart';
 import '../../core/failures/failures.dart';
 import '../repositories/space_x_repository.dart';
 
+
+// Use case for fetching launches.
+// This class abstracts the business logic from the repository layer.
 class FetchLaunchesUseCase {
   final SpaceXRepository spaceXRepository;
 
